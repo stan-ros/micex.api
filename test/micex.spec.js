ab7e2480e0246c4cefcf2e23f5c33dabb1e44c64
sklyukin <stas.msu@gmail.com>
2015-08-08 02:55:05 +0500

    markets method. better tests

--- test/micex.spec.js
+++ test/micex.spec.js
@@ -2,26 +2,32 @@ import Micex from '../lib/micex';
 let should = require('chai').should();
 
 describe('MICEX. ', () => {
-  it('General request', (done) => {
+  it('General request', () => {
     should.exist(Micex);
     should.exist(Micex._request);
-    Micex._request('engines')
+    return Micex._request('engines')
       .then((data) => {
         data.should.be.an('object');
         data.should.have.property('engines');
         data.engines.should.have.property('data');
-        done();
-      })
-      .catch((error) => {
-        console.console.error(error);
-        throw error;
       })
   });
-  it('Engines. Should be at least 3', (done) => {
-    Micex.engines()
+
+  it('Engines. Should be at least 3, should have stock engine.', () => {
+    return Micex.engines()
       .then((engines) => {
-          engines.should.have.length.least(6);
-          done();
+        engines.should.have.length.least(3);
+        let stockEngine = engines.find( engine => engine.name === 'stock');
+        should.exist(stockEngine);
       });
+  });
+
+  it('Markets. Should contains shares market for stock engine', () => {
+    return Micex.markets('stock')
+      .then((markets) => {
+        markets.should.have.length.least(5);
+        let sharesMarket = markets.find( market => market.NAME === 'shares');
+        should.exist(sharesMarket);
+      })
   });
 });
